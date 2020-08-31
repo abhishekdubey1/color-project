8bbe41c466f14d3eb5630a46cb02a4a4970b707e
Abhishek Dubey <54868382+ad99526@users.noreply.github.com>
2020-09-01 02:24:25 +0530

    Linked PaletteList to Palettes

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,19 +1,62 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import MiniPalette from "./MiniPalette";
+import { withStyles } from "@material-ui/styles";
 
-export default class PaletteList extends Component {
+const styles = {
+  root: {
+    backgroundColor: "blue",
+    height: "100vh",
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center"
+  },
+  container: {
+    width: "50%",
+    display: "flex",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    flexWrap: "wrap"
+  },
+  nav: {
+    display: "flex",
+    width: "100%",
+    justifyContent: "space-between"
+  },
+  palettes: {
+    boxSizing: "border-box",
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 30%)",
+    gridGap: "5%"
+  }
+};
+
+class PaletteList extends Component {
+  goToPalette(id) {
+    console.log("hi");
+    this.props.history.push(`/palette/${id}`);
+  }
   render() {
-    const { palettes } = this.props;
+    const { palettes, classes } = this.props;
     return (
-      <div>
-        <h2>
-          {palettes.map((palette) => (
-            <Link to={`/palette/${palette.id}`}>
-              <p>{palette.paletteName}</p>
-            </Link>
-          ))}
-        </h2>
+      <div className={classes.root}>
+        <div className={classes.container}>
+          <nav className={classes.nav}>
+            <h1>React Colors</h1>
+          </nav>
+          {palettes.length}
+          <div className={classes.palettes}>
+            {palettes.map((palette) => (
+              <MiniPalette
+                {...palette}
+                handleClick={() => this.goToPalette(palette.id)}
+                key={palette.id}
+              />
+            ))}
+          </div>
+        </div>
       </div>
     );
   }
 }
+export default withStyles(styles)(PaletteList);
