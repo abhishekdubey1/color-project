ac114372f19149bb272095103223d31d7ff7c701
Abhishek Dubey <54868382+ad99526@users.noreply.github.com>
2020-08-31 15:26:04 +0530

    fixed params id

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
         />
         <Route
           exact
-          path="/:id"
+          path="/palette/:id"
           render={(routeProps) => (
             <Palette
               palette={generatePalette(
